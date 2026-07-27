6a9737a9a212b24f9f2c63e8e1c9fe03d9f0355a
Prateek Sharma <prateek280614@gmail.com>
2026-07-27 19:33:10 +0530

    Added Day 1 DSA Assignment

---
+++ DAY1_Q1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int n, pos, x;
+
+    scanf("%d", &n);
+
+    int arr[n + 1];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &pos);
+    scanf("%d", &x);
+
+    // Shift elements to the right
+    for (int i = n; i >= pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    // Insert the new element
+    arr[pos - 1] = x;
+
+    // Print updated array
+    for (int i = 0; i <= n; i++) {
+        printf("%d", arr[i]);
+        if (i < n)
+            printf(" ");
+    }
+
+    return 0;
+}
