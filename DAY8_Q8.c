715dcd28d73afb3b524a841d56d0608d0983332b
Prateek Sharma <prateek280614@gmail.com>
2026-08-03 22:30:52 +0530

    Add DAY8_Q8.c - Recursive power program in C

---
+++ DAY8_Q8.c
@@ -0,0 +1,22 @@
+// Problem: Compute a^b using recursion without using pow() function.
+
+#include <stdio.h>
+
+// Recursive function to calculate power
+int power(int a, int b)
+{
+    if (b == 0)
+        return 1;
+    return a * power(a, b - 1);
+}
+
+int main()
+{
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    printf("%d", power(a, b));
+
+    return 0;
+}
